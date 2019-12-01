c22695683edc76c92b5c235a756f14525a778451
sariwahyu <sariwahyuningsih22@gmail.com>
2019-12-01 09:54:52 +0700

    camera position

---
+++ Assets/Scripts/backgroundLoop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class backgroundLoop : MonoBehaviour
+{
+    public Transform target;
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+
+    }
+}
