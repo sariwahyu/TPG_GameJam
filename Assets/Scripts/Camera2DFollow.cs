4f1a50250e5521ef218eea0c6b3cdf50cf2ff540
sariwahyu <sariwahyuningsih22@gmail.com>
2019-12-01 13:47:13 +0700

    respawn 2

--- Assets/Scripts/Camera2DFollow.cs
+++ Assets/Scripts/Camera2DFollow.cs
@@ -9,6 +9,7 @@ public class Camera2DFollow : MonoBehaviour
     public float lookAheadFactor = 3;
     public float lookAheadReturnSpeed = 0.5f;
     public float lookAheadMoveThreshold = 0.1f;
+    public float yPosRestriction = -1;
 
     float offsetZ;
     Vector3 lastTargetPosition;
@@ -20,5 +21,35 @@ public class Camera2DFollow : MonoBehaviour
         lastTargetPosition = target.position;
         offsetZ = (transform.position - target.position).z;
         transform.parent = null;
+    }
+
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        float xMoveDelta = (target.position - lastTargetPosition).x;
+
+        bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+
+        if (updateLookAheadTarget)
+        {
+            lookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign(xMoveDelta);
+        }
+        else
+        {
+            lookAheadPos = Vector3.MoveTowards(lookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);
+        }
+
+        Vector3 aheadTargetPos = target.position + lookAheadPos + Vector3.forward * offsetZ;
+        Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref currentVelocity, damping);
+
+        newPos = new Vector3(newPos.x, Mathf.Clamp(newPos.y, yPosRestriction, Mathf.Infinity), newPos.z);
+
+        transform.position = newPos;
+
+        lastTargetPosition = target.position;
     }
 }
