dbb9ab71ebbd6a19217688f51ab9f0efc6614bb0
sariwahyu <sariwahyuningsih22@gmail.com>
2019-12-02 12:48:29 +0700

    font + buat semua scene + button manager

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,21 @@ public class Player : MonoBehaviour
     [System.Serializable]
     public class PlayerStats
     {
-        public float Health = 100f;
+        public int maxHealth = 100;
+
+        private int _curHealth;
+        public int curHealth
+        {
+            get { return _curHealth; }
+            set { _curHealth = Mathf.Clamp(value, 0, maxHealth); }
+        }
     }
 
     public PlayerStats playerStats = new PlayerStats();
     public int fallBoundary = -20;
+
+    [SerializeField]
+    private StatusIndicator statusIndicator;
 
     void Update()
     {
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour
+{
+
+    public void LoadScene(string SceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+    }
+
+    public void SceneLoader(string SceneString)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneString);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
